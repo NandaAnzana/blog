b0aff889475c92072595139ae357ce7b9789c15d
Nanda Anzana <nanda.anzana@gmail.com>
2025-09-28 20:25:52 +0700

    [fix] add sentry config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  entry: {
+    reactComponentAnnotation: {
+      enabled: false,
+    },
+  },
 };
 
 export default nextConfig;
